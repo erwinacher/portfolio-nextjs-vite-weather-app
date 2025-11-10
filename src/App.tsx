70bc3f7065174e3549d9f0db64ed0d869a038ae5
erwinacher <erwin.acher@gmail.com>
2025-11-10 22:47:17 +0000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <div className="size-32 bg-red-500"></div>
+    </>
+  );
+}
+
+export default App;
+
+// done - styling - Tailwind.css
+// done - data fetching - TanStack Query (React Query)
