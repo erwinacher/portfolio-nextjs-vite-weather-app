988a61c250d4c4524d2b2c9118835d8fd56232a4
erwinacher <erwin.acher@gmail.com>
2025-11-10 23:54:47 +0000

    [API] added zod schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { useQuery } from "@tanstack/react-query";
 import { getWeather } from "./api";
+import type { WeatherResponse } from "./schemas/weatherSchema";
 
 function App() {
-  const { data } = useQuery({
+  const { data } = useQuery<WeatherResponse>({
     queryKey: ["weather"],
     queryFn: () => getWeather({ lat: 10, lon: 25 }),
   });
+  console.log(JSON.stringify(data, null, 2));
+  console.log(data?.current.temp);
   return (
     <>
-      <div>{JSON.stringify(data)}</div>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
   );
 }
--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,10 @@
 const API_KEY = import.meta.env.VITE_API_KEY;
+import { WeatherResponseSchema } from "./schemas/weatherSchema";
 
 export async function getWeather({ lat, lon }: { lat: number; lon: number }) {
   const res = await fetch(
     `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,alerts&appid=${API_KEY}`
   );
   const data = await res.json();
-  return data;
+  return WeatherResponseSchema.parse(data);
 }
